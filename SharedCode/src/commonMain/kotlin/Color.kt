7d56a9ec15bf84e485bc61576a8b6b0aab51f019
Rohen Giralt <rohengiralt@gmail.com>
2020-03-30 11:06:16 -0400

    Added factory function colorFromHSV, Moved HSVtoRGB from companion object to file private, Modified constructor parameters' order; alpha is now last.
    Signed-off-by: Rohen Giralt <rohengiralt@gmail.com>

--- SharedCode/src/commonMain/kotlin/Color.kt
+++ SharedCode/src/commonMain/kotlin/Color.kt
@@ -2,14 +2,12 @@ package com.jetbrains.debatex
 
 import kotlin.math.abs
 
-@ExperimentalUnsignedTypes
 data class Color(
-    var alpha: Double = 1.0,
     var red: Double,
     var green: Double,
-    var blue: Double
+    var blue: Double,
+    var alpha: Double = 1.0
 ) {
-
     private val greatestRGB
         get() = maxOf(red, green, blue)
 
@@ -58,35 +56,43 @@ data class Color(
             blue -> ((red - green) / rangeRGB) + 4
             else -> throw IllegalStateException("Color mutated while calculating hue.")
         } / 6
+}
 
-    companion object {
-        fun HSVtoRGB(
-            hue: Double,
-            saturation: Double,
-            value: Double
-        ): Triple<Double, Double, Double> {
-            if (hue !in 0.0..1.0) {
-                throw IllegalArgumentException("Hue must be between 0 and 1")
-            }
-            if (saturation !in 0.0..1.0) {
-                throw IllegalArgumentException("Saturation must be between 0 and 1")
-            }
-            if (value !in 0.0..1.0) {
-                throw IllegalArgumentException("Value must be between 0 and 1")
-            }
+fun colorFromHSV(
+    hue: Double,
+    saturation: Double,
+    value: Double,
+    alpha: Double = 1.0
+): Color {
+    val (red, green, blue) = HSVtoRGB(hue, saturation, value)
+    return Color(alpha, red, green, blue)
+}
 
-            val chroma = value * saturation
-            val x = chroma * (1 - abs((hue * 6) % 2 - 1))
-            val m = value - chroma
-            return when (hue * 6) {
-                in 0.0..1.0 -> Triple(chroma, x, 0.0)
-                in 1.0..2.0 -> Triple(x, chroma, 0.0)
-                in 2.0..3.0 -> Triple(0.0, chroma, x)
-                in 3.0..4.0 -> Triple(0.0, x, chroma)
-                in 4.0..5.0 -> Triple(x, 0.0, chroma)
-                in 5.0..6.0 -> Triple(chroma, 0.0, x)
-                else -> throw xkcdException() //TODO: Throw a real exception
-            }.let { Triple(it.first + m, it.second + m, it.third + m) }
-        }
+private fun HSVtoRGB(
+    hue: Double,
+    saturation: Double,
+    value: Double
+): Triple<Double, Double, Double> {
+    if (hue !in 0.0..1.0) {
+        throw IllegalArgumentException("Hue must be between 0 and 1")
     }
+    if (saturation !in 0.0..1.0) {
+        throw IllegalArgumentException("Saturation must be between 0 and 1")
+    }
+    if (value !in 0.0..1.0) {
+        throw IllegalArgumentException("Value must be between 0 and 1")
+    }
+
+    val chroma = value * saturation
+    val x = chroma * (1 - abs((hue * 6) % 2 - 1))
+    val m = value - chroma
+    return when (hue * 6) {
+        in 0.0..1.0 -> Triple(chroma, x, 0.0)
+        in 1.0..2.0 -> Triple(x, chroma, 0.0)
+        in 2.0..3.0 -> Triple(0.0, chroma, x)
+        in 3.0..4.0 -> Triple(0.0, x, chroma)
+        in 4.0..5.0 -> Triple(x, 0.0, chroma)
+        in 5.0..6.0 -> Triple(chroma, 0.0, x)
+        else -> throw xkcdException() //TODO: Throw a real exception
+    }.let { Triple(it.first + m, it.second + m, it.third + m) }
 }
